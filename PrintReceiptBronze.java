0c76a9b21d3de659bd383fb0edd907f23ccb600d
Toto <109338896+goldentoto@users.noreply.github.com>
2023-11-16 12:44:07 -0700

    Delete PrintReceiptBronze.java

--- PrintReceiptBronze.java
+++
@@ -1,8 +0,0 @@
-package com.thelocalmarketplace.software;
-
-import com.jjjwelectronics.printer.ReceiptPrinterBronze;
-
-public class PrintReceiptBronze {
-	private ReceiptPrinterBronze printer; 
-
-}
