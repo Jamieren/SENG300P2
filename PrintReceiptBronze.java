d0b7ca4829719850e19cfaddf91d6e312340a2ba
Toto <109338896+goldentoto@users.noreply.github.com>
2023-11-16 12:37:36 -0700

    Add files via upload

---
+++ PrintReceiptBronze.java
@@ -0,0 +1,8 @@
+package com.thelocalmarketplace.software;
+
+import com.jjjwelectronics.printer.ReceiptPrinterBronze;
+
+public class PrintReceiptBronze {
+	private ReceiptPrinterBronze printer; 
+
+}
