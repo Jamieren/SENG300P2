038c1a8b90ce84727345a293cc6a03942f296674
Hillary Nguyen <hnguy@10.0.0.46>
2023-11-17 21:37:27 -0700

    hardware updated

---
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/PayDebitTest.java
@@ -0,0 +1,19 @@
+package com.thelocalmarketplace.software.test;
+
+import org.junit.Before;
+
+import com.jjjwelectronics.card.Card;
+import com.thelocalmarketplace.software.SelfCheckoutStationSoftware;
+import com.thelocalmarketplace.software.Session;
+
+public class PayDebitTest {
+
+	private Card card;
+	private Session session;
+	private SelfCheckoutStationSoftware station;
+	
+	@Before
+	public void setup() {
+		
+	}
+}
