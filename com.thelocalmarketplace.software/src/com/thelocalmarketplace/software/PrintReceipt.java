a793dafe8c13f161b7016412cb5abe6040736529
Toto <109338896+goldentoto@users.noreply.github.com>
2023-11-19 21:06:16 -0700

    Add files via upload

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PrintReceipt.java
@@ -0,0 +1,93 @@
+package com.thelocalmarketplace.software;
+
+import com.jjjwelectronics.IDevice;
+import com.jjjwelectronics.IDeviceListener;
+import com.jjjwelectronics.printer.ReceiptPrinterBronze;
+import com.jjjwelectronics.printer.ReceiptPrinterListener;
+
+public class PrintReceipt implements ReceiptPrinterListener {
+	
+	    private double amountDue;
+		private ReceiptPrinterBronze receiptPrinter;	    
+	     
+	    public void setAmountDue(Session session) {
+	    	amountDue = session.getAmountDue();
+	    }
+
+	    public void print() {
+	        try {
+	            // Print the amount due
+	            String amountDueString = String.format("Amount Due: $%.2f", amountDue);
+	            for (char c : amountDueString.toCharArray()) {
+	                receiptPrinter.print(c);
+	                
+	             // Print a newline to separate the payment record and amount due
+		            receiptPrinter.print('\n');
+
+
+	            }
+
+
+	        } catch (Exception e) {
+	            // Handle exceptions, e.g., out of paper or ink
+	            System.err.println("Printing failed: " + e.getMessage());
+	        }
+	    }
+
+	    // ReceiptPrinterListener methods
+	    @Override
+	    public void thePrinterIsOutOfInk() {
+	        System.out.println("Printer is out of ink. Printing aborted.");
+	        // Additional handling logic can be added here
+	    }
+
+	    @Override
+	    public void thePrinterIsOutOfPaper() {
+	        System.out.println("Printer is out of paper. Printing aborted.");
+	        // Additional handling logic can be added here
+	    }
+
+	    @Override
+	    public void inkHasBeenAddedToThePrinter() {
+	        System.out.println("Ink has been added to the printer. Ready to print.");
+	        // Additional handling logic can be added here
+	    }
+
+	    @Override
+	    public void paperHasBeenAddedToThePrinter() {
+	        System.out.println("Paper has been added to the printer. Ready to print.");
+	        // Additional handling logic can be added here
+	    }
+		@Override
+		public void aDeviceHasBeenEnabled(IDevice<? extends IDeviceListener> device) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void aDeviceHasBeenDisabled(IDevice<? extends IDeviceListener> device) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void aDeviceHasBeenTurnedOn(IDevice<? extends IDeviceListener> device) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void aDeviceHasBeenTurnedOff(IDevice<? extends IDeviceListener> device) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void thePrinterHasLowInk() {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void thePrinterHasLowPaper() {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+
+
