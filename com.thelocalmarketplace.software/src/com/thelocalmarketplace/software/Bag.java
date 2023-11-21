82c9898f924ea3db9125f818108ef2289def2030
Jamie Ren <116605795+Jamieren@users.noreply.github.com>
2023-11-20 21:23:10 -0700

    Update Bag.java

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Bag.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Bag.java
@@ -1,5 +1,20 @@
 package com.thelocalmarketplace.software;
 
+/*SENG 300 Project Iteration 2
+
+@author Akashdeep Grewal 30179657
+@author Amira Wishah 30182579
+@author Ananya Jain 30196069
+@author Danny Ly 30127144
+@author Hillary Nguyen 30161137
+@author Johnny Tran 30140472 
+@author Minori Olguin 30035923
+@author Rhett Bramfield 30170520
+@author Wyatt Deichert 30174611
+@author Zhenhui Ren 30139966
+@author Adrian Brisebois 30170764
+*/
+
 import com.jjjwelectronics.Item;
 import com.jjjwelectronics.Mass;
 
